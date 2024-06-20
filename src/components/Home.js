8320c648e4cf4eb47e2567f141bfea8ab3b6319a
Ryan <torrance.jr@gmail.com>
2024-06-20 12:44:56 -0400

    changed images

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,3 @@
-// src/components/Home.js
 import React, { useState } from 'react';
 import { Dialog, DialogPanel } from '@headlessui/react';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
@@ -11,15 +10,20 @@ const navigation = [
 ];
 
 export default function Home() {
-    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false); // Menu is closed by default
 
     return (
         <div className="bg-white">
             <header className="absolute inset-x-0 top-0 z-50 lg:top-8">
                 <nav className="flex items-center justify-between p-4 lg:px-8" aria-label="Global">
-                    <div className="flex lg:flex-1">
+                    <div className="flex items-center lg:flex-1">
                         <a href="/" className="-m-1.5 p-1.5">
                             <span className="sr-only">WixenCo</span>
+                            <img
+                                className="h-12 w-auto"
+                                src="https://wixenco.s3.amazonaws.com/ryan%40wixenco.com.svg"
+                                alt="WixenCo Logo"
+                            />
                         </a>
                     </div>
                     <div className="flex lg:hidden">
@@ -42,7 +46,7 @@ export default function Home() {
                     <div className="hidden lg:flex lg:flex-1 lg:justify-end">
                     </div>
                 </nav>
-                <Dialog className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+                <Dialog className="lg:hidden" open={mobileMenuOpen} onClose={() => setMobileMenuOpen(false)}>
                     <div className="fixed inset-0 z-50" />
                     <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-3 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                         <div className="flex items-center justify-between">
@@ -50,8 +54,8 @@ export default function Home() {
                                 <span className="sr-only">WixenCo</span>
                                 <img
                                     className="h-32 w-auto"
-                                    src="https://wixenco.s3.amazonaws.com/wix-logo.png"
-                                    alt=""
+                                    src="https://wixenco.s3.amazonaws.com/ryan%40wixenco.com.svg"
+                                    alt="WixenCo Logo"
                                 />
                             </a>
                             <button
@@ -107,14 +111,10 @@ export default function Home() {
                     </svg>
                     <rect width="100%" height="100%" strokeWidth={0} fill="url(#83fd4e5a-9d52-42fc-97b6-718e5d7ee527)" />
                 </svg>
-                <div className="mx-auto max-w-7xl px-6 py-16 sm:py-32 lg:flex lg:items-center lg:gap-x-10 lg:px-8 lg:py-40">
+                <div className="mx-auto max-w-7xl px-6 py-16 sm:py-32 lg:py-40 lg:flex lg:items-center lg:gap-x-10 lg:px-8">
                     <div className="mx-auto max-w-2xl lg:mx-0 lg:flex-auto">
-                        <div className="flex">
-
-                        </div>
                         <h1 className="mt-10 max-w-lg text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
                             A <span className="text-transparent bg-clip-text bg-gradient-to-r from-teal-300 to-purple-500">Freelance</span> Web Developer Crafting Websites That Tell Your Story
-
                         </h1>
                         <p className="mt-6 text-lg leading-8 text-gray-600">
                             I specialize in creating unique, tailored websites that effectively communicate your brand's narrative and engage your audience.
@@ -131,37 +131,16 @@ export default function Home() {
                             </a>
                         </div>
                     </div>
-                    <div className="hidden sm:block mt-8 sm:mt-24 lg:mt-0 lg:flex-shrink-0 lg:flex-grow">
-                        <svg viewBox="0 0 366 729" role="img" className="mx-auto w-[22.875rem] max-w-full drop-shadow-xl">
-                            <title>App screenshot</title>
-                            <defs>
-                                <clipPath id="2ade4387-9c63-4fc4-b754-10e687a0d332">
-                                    <rect width={316} height={684} rx={36} />
-                                </clipPath>
-                            </defs>
-                            <path
-                                fill="#4B5563"
-                                d="M363.315 64.213C363.315 22.99 341.312 1 300.092 1H66.751C25.53 1 3.528 22.99 3.528 64.213v44.68l-.857.143A2 2 0 0 0 1 111.009v24.611a2 2 0 0 0 1.671 1.973l.95.158a2.26 2.26 0 0 1-.093.236v26.173c.212.1.398.296.541.643l-1.398.233A2 2 0 0 0 1 167.009v47.611a2 2 0 0 0 1.671 1.973l1.368.228c-.139.319-.314.533-.511.653v16.637c.221.104.414.313.56.689l-1.417.236A2 2 0 0 0 1 237.009v47.611a2 2 0 0 0 1.671 1.973l1.347.225c-.135.294-.302.493-.49.607v377.681c0 41.213 22 63.208 63.223 63.208h95.074c.947-.504 2.717-.843 4.745-.843l.141.001h.194l.086-.001 33.704.005c1.849.043 3.442.37 4.323.838h95.074c41.222 0 63.223-21.999 63.223-63.212v-394.63c-.259-.275-.48-.796-.63-1.47l-.011-.133 1.655-.276A2 2 0 0 0 366 266.62v-77.611a2 2 0 0 0-1.671-1.973l-1.712-.285c.148-.839.396-1.491.698-1.811V64.213Z"
-                            />
-                            <path
-                                fill="#343E4E"
-                                d="M16 59c0-23.748 19.252-43 43-43h246c23.748 0 43 19.252 43 43v615c0 23.196-18.804 42-42 42H58c-23.196 0-42-18.804-42-42V59Z"
-                            />
-                            <foreignObject
-                                width={316}
-                                height={684}
-                                transform="translate(24 24)"
-                                clipPath="url(#2ade4387-9c63-4fc4-b754-10e687a0d332)"
-                            >
-                                <img src="https://wixenco.s3.amazonaws.com/wixen-phone2.png" alt="" />
-                            </foreignObject>
-                        </svg>
-                    </div>
-                    <div className="block sm:hidden mt-8 sm:mt-24 lg:mt-0 lg:flex-shrink-0 lg:flex-grow">
-                        <img src="https://wixenco.s3.amazonaws.com/moonman.svg" alt="Moonman" className="mx-auto w-[22.875rem] max-w-full drop-shadow-xl" />
+                    <div className="mt-8 sm:mt-24 lg:mt-0 lg:flex-shrink-0 lg:flex-grow flex justify-center">
+                        <img src="https://wixenco.s3.amazonaws.com/moonman2.svg" alt="Moonman" className="hidden lg:block w-[22.875rem] max-w-full drop-shadow-xl" />
+                        <img src="https://wixenco.s3.amazonaws.com/moonman.svg" alt="Moonman" className="lg:hidden w-[22.875rem] max-w-full drop-shadow-xl" />
                     </div>
                 </div>
             </div>
         </div>
     )
 }
+
+
+
+
