36070c072990ae224b5832c53a80038a10d0af25
Ryan <torrance.jr@gmail.com>
2024-08-27 13:18:07 -0400

    website blog post

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ import CssFrameworks from "./features/Blog/articles/CssFrameworks";
 import ChatGptJulyTwentyFour from "./features/Blog/articles/ChatGptJulyTwentyFour";
 import TopBusinessDirectories from "./features/Blog/articles/TopBusinessDirectories";
 import ChristmasParadeTwentyFour from "./features/Blog/articles/ChristmasParadeTwentyFour";
+import TopTenImageSites from "./features/Blog/articles/TopTenImageSites";
 import { createBrowserRouter, RouterProvider} from "react-router-dom";
 import ReactGA from 'react-ga4';
 
@@ -35,6 +36,7 @@ const router = createBrowserRouter([
     { path: '/blog/top-10-business-directories-to-boost-seo-online-presence', element: <TopBusinessDirectories />},
     { path: '/blog/chatgpt-upgraded-voice-mode-coming-soon', element: <ChatGptJulyTwentyFour />},
     { path: '/blog/west-chester-christmas-parade-2024', element: <ChristmasParadeTwentyFour />},
+    { path: '/blog/top-10-free-website-image-websites', element: <TopTenImageSites />},
     { path: '*', element: <NotFoundPage /> },
 ]);
 
